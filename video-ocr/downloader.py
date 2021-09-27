799e1e9373e7f04f674cbd214c2347d1a7adc7c6
Hyejin Shin <jstep0000@gmail.com>
2021-09-28 04:53:06 +0900

    Add files via upload

---
+++ video-ocr/downloader.py
@@ -0,0 +1,37 @@
+import os
+import uuid
+
+from pytube import YouTube
+
+def move_video_to(internal_video_path, destination_dir):
+    
+    # Get the Video's MD5 sum and ensure that it does not exist already
+    video_md5sum = internal_video_path
+    expected_video_path = os.path.join(destination_dir, video_md5sum + ".mp4")
+    if not os.path.exists(destination_dir):     # create destination_dir
+        os.makedirs(destination_dir)
+    if not os.path.exists(expected_video_path):     # move file if not exists on destination
+        os.rename(internal_video_path, expected_video_path)
+    else:
+        print("File with md5sum '{}' already exists on destination folder"
+                      .format(video_md5sum))
+    return video_md5sum
+
+
+
+url = 'https://www.youtube.com/watch?v=eW878Rmr8wY'
+file_md5sum = uuid.uuid4().hex
+
+youtube = YouTube(url)
+
+youtube = YouTube(url)
+stream = youtube.streams.filter(adaptive=True).first()
+
+vidpath = os.path.join(os.getcwd(),'Downloads')
+print(vidpath)
+stream.download(vidpath, file_md5sum)
+
+file_md5sum = move_video_to(
+    os.path.join(vidpath, file_md5sum),vidpath)
+
+print('end')
---
+++ video-ocr/testocr.py
@@ -0,0 +1,45 @@
+#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Created on Wed Nov 28 12:33:08 2018
+
+@author: jordansauchuk
+'''
+
+from PIL import Image
+import pytesseract
+from wand.image import Image as Img
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+import numpy as np
+
+
+string = ''
+
+for i in range(10,20):
+    frame = './image_frames2/frame' + str(i) + '.0.png'
+    demo = Image.open(frame)
+    text = pytesseract.image_to_string(demo, lang = 'eng')
+    string = string + text
+
+print(string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
